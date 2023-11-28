d4816d486da6c3c277647ae169c4c3fe64939d9c
Dario Semino <orbislibroscontacto@gmail.com>
2023-11-28 14:23:25 -0300

    cartel actualizado

--- src/components/sitio/CartelPrueba.jsx
+++ src/components/sitio/CartelPrueba.jsx
@@ -20,17 +20,11 @@ export default function CartelPrueba() {
                 </Modal.Header>
                 <Modal.Body> <p>
                     La presente es una muestra del sitio web de la Asociación Gerontovida. La información y los datos aquí presentados son generados por funciones random, en ningún caso representan datos reales. </p>
-                    <hr />
 
-                    <h5> ¿Qué se puede hacer aquí? </h5>  <p>
-                        En esta muestra del sitio es posible realizar las operaciones para registrarse como usuario y acceder al escritorio para publicar, editar o eliminar artículos de la sección de noticias. También es posible recuperar contraseña y acceder a la información del usuario loggeado. La autenticación de usuario y sesión se realiza mediante token generado con JWT.<br />
-                        En la sección Canasta es posible publicar, editar o eliminar cualquiera de las Canastas, también se puede subir un archivo PDF con la información de la Canasta, el cual a su vez se puede descargar. <br />
-                        La información que aparece en el resto de las secciones (actividades, quiénes somos, etc) están harcodeadas en el front-end y no se pueden modificar.
-                    </p>
                     <hr />
                     <h5>Información técnica</h5>
                     <p>
-                        El front-end está desarrollado con React, Redux, ReactBootstrap, Styled-Components y React-Chart para los gráficos de la canasta, entre otras tecnologías. Toda la sección pública del sitio es responsive. La sección interna, correspondiente al Login y el acceso al escritorio de trabajo no es responsive.
+                        El front-end está desarrollado con React, Redux, ReactBootstrap, Styled-Components y React-Chart para los gráficos de la canasta, entre otras tecnologías. Toda la sección pública del sitio es responsive. La sección interna, correspondiente al Login y el acceso al escritorio de trabajo no es responsive. Y no es tampoco de acceso público.
 
                         El front-end se encuentra alojado en Netlify.
                     </p>
